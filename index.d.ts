485daaec0a819ff4ea2306ef05368de3c32122c7
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-06-27 16:27:53 -0700

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Converts an item to a string.
+ * @param item The data to stringify.
+ */
+export function stringify(item:any):string
+
+/**
+ * Retrives an item from the stringified
+ * @param data The data to parse.
+ */
+export function parse(data:string):any
+
